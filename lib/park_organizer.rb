7a7a1701d1a295cf318bef616f33f4cbd56aabf9
Rob Hill <robert.hill0122@gmail.com>
2014-07-14 10:26:44 -0600

    still working to create array from values with shared key

--- lib/park_organizer.rb
+++ lib/park_organizer.rb
@@ -8,18 +8,22 @@ class ParkOrganizer
 
   def sort_by_id
     sorted_parks = {}
-    @parks.each do |park|
+    parks = @parks.sort_by {|hash| hash[:id]}
+    parks.each do |park|
     sorted_parks[park[:id]] = park
     end
     sorted_parks
   end
 
   def sort_by_country
+    #working to figure out how to make parks with same country appear in an array together
     sorted_parks = {}
-    @parks.each do |park|
+    parks = @parks.sort_by {|hash| hash[:country]}
+    parks.each do |park|
       sorted_parks[park[:country]] = [park]
     end
-    sorted_parks.sort.flatten
+    sorted_parks
   end
 
 end
+
