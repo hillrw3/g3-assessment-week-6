2a20e5d599a6f22f09a37fb3bddf83cbfbdd1764
Rob Hill <robert.hill0122@gmail.com>
2014-07-14 09:48:37 -0600

    Sort by id completed

--- lib/park_organizer.rb
+++ lib/park_organizer.rb
@@ -1,8 +1,25 @@
 class ParkOrganizer
 
-  def sort_by_id (id)
+  def initialize (parks)
+    @parks = parks
+  end
 
 
+
+  def sort_by_id
+    sorted_parks = {}
+    @parks.each do |park|
+    sorted_parks[park[:id]] = park
+    end
+    sorted_parks
+  end
+
+  def sort_by_country
+    sorted_parks = {}
+    @parks.each do |park|
+      sorted_parks[park[:country]] = [park]
+    end
+    sorted_parks.sort.flatten
   end
 
 end
--- spec/park_organizer_spec.rb
+++ spec/park_organizer_spec.rb
@@ -2,22 +2,22 @@ require 'spec_helper'
 
 describe ParkOrganizer do
 
-  parks = [
-    {
-      :id=>546,
-      :name=>"Kalahari Resorts",
-      :city=>"Wisconsin Dells",
-      :state=>"Wisconsin",
-      :country=>"United States"
-    },
-    {
-      :id=>547,
-      :name=>"Little Amerricka",
-      :city=>"Marshall",
-      :state=>"Wisconsin",
-      :country=>"United States"
-    }
-  ]
+  parks = ParkOrganizer.new([
+                              {
+                                :id=>546,
+                                :name=>"Kalahari Resorts",
+                                :city=>"Wisconsin Dells",
+                                :state=>"Wisconsin",
+                                :country=>"United States"
+                              },
+                              {
+                                :id=>547,
+                                :name=>"Little Amerricka",
+                                :city=>"Marshall",
+                                :state=>"Wisconsin",
+                                :country=>"United States"
+                              }
+                            ])
 
   parks_by_id = {
     546 => {
@@ -39,4 +39,62 @@ describe ParkOrganizer do
   it "sorts the parks by id" do
     expect(parks.sort_by_id).to eq(parks_by_id)
   end
+
+  parks2 = ParkOrganizer.new([
+                               {
+                                 :id=>546,
+                                 :name=>"Kalahari Resorts",
+                                 :city=>"Wisconsin Dells",
+                                 :state=>"Wisconsin",
+                                 :country=>"United States"
+                               },
+                               {
+                                 :id=>547,
+                                 :name=>"Little Amerricka",
+                                 :city=>"Marshall",
+                                 :state=>"Wisconsin",
+                                 :country=>"United States"
+                               },
+                               {
+                                 :id=>2,
+                                 :name=>"Calaway Park",
+                                 :city=>"Calgary",
+                                 :state=>"Alberta",
+                                 :country=>"Canada"
+                               }
+                             ])
+
+  parks_by_country = {
+    "Canada" => [
+      {
+        :id=>2,
+        :name=>"Calaway Park",
+        :city=>"Calgary",
+        :state=>"Alberta",
+        :country=>"Canada"
+      }
+    ],
+    "United States" => [
+      {
+        :id=>546,
+        :name=>"Kalahari Resorts",
+        :city=>"Wisconsin Dells",
+        :state=>"Wisconsin",
+        :country=>"United States"
+      },
+      {
+        :id=>547,
+        :name=>"Little Amerricka",
+        :city=>"Marshall",
+        :state=>"Wisconsin",
+        :country=>"United States"
+      }
+    ]
+  }
+
+  it "sorts the parks by country" do
+    expect(parks2.sort_by_country).to eq(parks_by_country)
+  end
+
+
 end
